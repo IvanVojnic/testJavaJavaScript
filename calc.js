049e702cdb233f5eaf3d23a5546017d69d70ba8f
IvanVojnic <ivojnic44@gmail.com>
2022-07-08 17:21:05 +0300

    feat: realise basic form

---
+++ calc.js
@@ -0,0 +1,27 @@
+let fieldInput = document.getElementById("inputCalc");
+let buttonSub = document.getElementById("buttonSub");
+buttonSub.click(function (){
+    prepareData();
+})
+function prepareData(){
+    let inpText = fieldInput.innerHTML;
+    $.ajax({
+        url: "app.java",
+        type: "POST",
+        async: false,
+        data: {jsonFilter: JSON.stringify(inpText), action: "calcVal"}
+    })
+        .done( function(data,  textStatus, jqXHR) {
+            showResult(data);
+        })
+        .fail( function( data ) {
+            console.log('fail');
+            console.log(data);
+        })
+
+}
+function showResult(data){
+    let result = JSON.parse(data);
+    let out = '<p>$result</p>';
+    $('.result').html(out);
+}
